df4e1981f89751e53d27ac67294b8dfbc32c54b9
nftchance <chance@utc24.io>
2024-07-14 17:43:09 -0500

    feat: rate limit

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,29 @@
 import cors from "cors"
 import express from "express"
+import { rateLimit } from "express-rate-limit"
 import http from "http"
 
 import { createExpressMiddleware } from "@trpc/server/adapters/express"
 
 import { Router, router } from "@/src/api"
 import { env } from "@/src/lib/utils/env"
-import { limiter } from "@/src/server/limiter"
 import { createContext } from "@/src/server/trpc"
 
 const app = express()
 const server = http.createServer(app)
 
 app.use(cors())
-app.use(limiter)
+app.use(
+	rateLimit({
+		standardHeaders: true,
+		legacyHeaders: false,
+		windowMs: env.RATE_LIMIT_WINDOW_MS,
+		limit: env.RATE_LIMIT,
+		skip: req =>
+			req.ip ? env.RATE_LIMIT_ALLOW_LIST.includes(req.ip) : false,
+		message: env.RATE_LIMIT_MESSAGE
+	})
+)
 app.use(
 	"/",
 	createExpressMiddleware<Router>({
